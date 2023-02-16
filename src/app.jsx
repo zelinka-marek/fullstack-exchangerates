935303a5f55bef4c0a5484eb6d2a4b097a5a21ff
Marek Zelinka <mzelinka17@gmail.com>
2023-02-16 11:35:44 +0100

    load exchange rates

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,54 @@
+import { useState } from "react";
+
 export function App() {
-  return <div></div>;
+  const [currency, setCurrency] = useState("");
+  const [rates, setRates] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (currency.trim().length) {
+      fetch(`https://open.er-api.com/v6/latest/${currency}`)
+        .then((response) => response.json())
+        .then((data) => setRates(data.rates));
+    }
+  };
+
+  return (
+    <div className="app">
+      <main>
+        <h1>Exchange Rates</h1>
+        <h2>Search by currency</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            required
+            value={currency}
+            onChange={(event) => setCurrency(event.target.value)}
+            aria-label="Currency"
+          />
+          <button type="submit">Search</button>
+        </form>
+        <h2>Results</h2>
+        {rates ? (
+          <table>
+            <tbody>
+              {Object.keys(rates).map((rate) => (
+                <tr key={rate}>
+                  <th style={{ textAlign: "left", paddingRight: 16 }}>
+                    {rate}
+                  </th>
+                  <td>{rates[rate]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <i>No exchange rates loaded yet. Try searching first</i>
+        )}
+      </main>
+      <footer>
+        <i>ExchangeRates &copy; 2023 Marek Zelinka</i>
+      </footer>
+    </div>
+  );
 }
